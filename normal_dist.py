af444313cd3029150c3d83d08708773e08728240
JacksonMHoke <jacksonmhoke@gmail.com>
2024-04-05 01:24:12 -0700

    Added CelebA dataset automatic download on run of normal_dist.py if not already present

--- normal_dist.py
+++ normal_dist.py
@@ -1,7 +1,7 @@
-from torch import randn_like
-from torch import tensor
 from torch import normal
+from torchvision import datasets
 import matplotlib.pyplot as plt
+from IPython.display import display
 mean=0
 std=1
 
@@ -9,3 +9,11 @@ samples=normal(mean, std, size=(1, 1000))
 
 plt.hist(samples)
 plt.show()
+
+celebs=datasets.CelebA(root='./celeb/', download=True)
+
+print(len(celebs))
+
+print(celebs[0])
+print(type(celebs[0]))
+display(celebs[0])
